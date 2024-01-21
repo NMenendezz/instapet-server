021068ef4744228c85507e2202477bf80c0892a8
Nacho Menéndez <nchmnndzlpz@gmail.com>
2024-01-21 17:48:33 +0100

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://instapet-client-378e2acf048a.herokuapp.com'
+  'https://instapet-client-jyujzy0aa-nmenendezz.vercel.app'
 ]
 
 export default allowedOrigins
